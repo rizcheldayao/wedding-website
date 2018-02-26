e7ad0eedd596d4088bd8977c4472c631672e20f2
Rizchel Dayao <rdayao@us.ibm.com>
2018-02-26 13:12:52 -0700

    initial commit

---
+++ src/containers/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+ class Layout extends Component {
+  render() {
+
+    return (
+      <div className="layout">
+        <h1>Wedding website</h1>
+      </div>
+    );
+  }
+}
+export default Counter;
