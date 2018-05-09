5d1049fb94782237425a7eb4fa2792e5b338928c
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2018-05-09 19:04:33 +0200

    Revert

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Engagement33 from '../images/Engagement-0033.jpg';
 
 class Footer extends Component {
   render () {
     return (
       <div className='footer'>
-        <img src={'https://lh3.googleusercontent.com/m-4BzHWWcLYVOdzhe-Ej_Mut6Gb8YcvaPwH_3t5qzY_aC_XUigUw_aK_nSbdVNtyvkZYK6ZfOxjV2wnun-ZY5r6ZgwFDwAeoEwYGhPVtgN8uducyxJiELtfasTHGmjX62MjuSIevJsnryxFhrnZ2c4YMyBH_iWQeveBOXBCaNxhpwjx5U37QOHno-N7KR2J73m_XgMYlHK3-NkcBKQ-P7JCYgX4xTTUeIWUh9BGNYWLp8q7AZ-an5T5s8AnI36eimDtloj2vYhI7LcfrHh7s4mGbc5uL2yVNTfPYmB9B9fUmt3jrzs2D27FOCxKBmDDbpOrB-Fyd0Otq8NsWj-nNZJ0D28C9TbLn94fST16YSaPurmlOP6V26YEgGVsE0OzOpTGddQfw9evLbo128lYhwYn0wqQSu86jt-TH556R2vFWD-xXY4aT3PWtEntrqXBjx90C9V-1VX2gqZBg8i19rqQpBASU3BADHupQipPoWCFtboH4JRQEzz0R7w34QS7WshMn7qhkxHfLPuyXomOrTYfDzi1d3POroQzWt-06GvOJkFHaXkOYHDwGeHwBhGuznWGjgBhV9UOphHOXd6ErQ74daSCkR2fAPX2vfxA=w2880-h512-no'} alt='Footer Image' />
+        <img src={Engagement33} alt='Footer Image' />
         <h1>Made with love by Rizchel</h1>
       </div>
     );
